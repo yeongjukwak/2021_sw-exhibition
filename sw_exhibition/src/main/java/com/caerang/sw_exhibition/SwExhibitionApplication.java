79fe8dc84db16bb3701190123b5e3af5481bacda
icekwak <yeongju.kwak@gmail.com>
2021-10-08 23:54:35 +0900

    2021.10.08 프로젝트 개발환경 업데이트

---
+++ sw_exhibition/src/main/java/com/caerang/sw_exhibition/SwExhibitionApplication.java
@@ -0,0 +1,13 @@
+package com.caerang.sw_exhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwExhibitionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwExhibitionApplication.class, args);
+    }
+
+}
---
+++ sw_exhibition/src/test/java/com/caerang/sw_exhibition/SwExhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caerang.sw_exhibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwExhibitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
